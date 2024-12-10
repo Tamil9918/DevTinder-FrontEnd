47cb40733023b08b81bf309effc407c7ac6deeab
Tamil9918 <tstamil9918@gmail.com>
2024-12-10 17:00:03 +0530

    userCard

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,6 +7,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [emailId, setEmailId] = useState("tamil123@gmail.com");
   const [password, setPassword] = useState("Test@123##");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleLogin = async () => {
@@ -22,6 +23,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       navigate("/");
     } catch (error) {
+      setError(error?.response?.data || "Someting went wrong!");
       console.log("Login Error:", error);
     }
   };
@@ -58,6 +60,7 @@ const Login = () => {
               />
             </label>
           </div>
+          <p className="text-red-800">{error}</p>
           <div className="card-actions justify-center m-2">
             <button className="btn text-white" onClick={() => handleLogin()}>
               Login
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,28 @@
+const userCard = ({ user }) => {
+  const { firstName, lastName, photoUrl, about, skills } = user;
+  return (
+    <div className="card bg-base-200 w-96 shadow-xl">
+      <figure>
+        <img
+          //   src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          //   alt="Shoes"
+          src={photoUrl}
+          alt={firstName}
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title justify-center">
+          {firstName + " " + lastName}
+        </h2>
+        <p className="flex justify-center">{about}</p>
+        <div className="card-actions justify-center my-4">
+          <button className="btn btn-primary">Ignore</button>
+
+          <button className="btn btn-secondary">Interested</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default userCard;
